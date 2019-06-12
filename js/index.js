98a103691332533fd564f3dcb77f7c8669860fe5
valeria campilongo <cvaleriac@hotmail.it>
2019-06-12 18:35:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
