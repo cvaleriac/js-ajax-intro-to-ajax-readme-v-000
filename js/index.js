2673a2a17393bf37317a4c45c9d385db77a8160d
valeria campilongo <cvaleriac@hotmail.it>
2019-06-12 18:36:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,3 +16,13 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
